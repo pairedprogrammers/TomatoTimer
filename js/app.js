d69dbc77de05115f065a612b7ee50fcb79468702
Paul Welden <paul.j.welden@gmail.com>
2015-01-12 21:02:30 -0600

    initial commit

---
+++ js/app.js
@@ -0,0 +1,12 @@
+var app = angular.module('tomatotimer', ['ngRoute'])
+    .config(function ($routeProvider) {
+        'use strict';
+
+        $routeProvider.when('/', {
+            controller: 'TimerCtrl',
+            templateUrl: 'views/timer.html'
+        })
+    })
+    .controller('TimerCtrl',[function(){
+
+    }]);
