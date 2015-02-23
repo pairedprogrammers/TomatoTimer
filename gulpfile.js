6537a959ae2462ddd9096f372385b04a057f30d0
Paul Welden <paul.j.welden@gmail.com>
2015-02-22 21:23:12 -0600

    setup asset injection

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,10 @@ var gulp = require('gulp'),
     uglify = require('gulp-uglify'),
     jshint = require('gulp-jshint'),
     clean = require('gulp-clean'),
-    inject = require('gulp-inject');
+    inject = require('gulp-inject'),
+    assets = require('./config/assets.json');
+
+
 
 
 gulp.task('lint', function () {
@@ -12,14 +15,25 @@ gulp.task('lint', function () {
         .pipe(jshint.reporter('jshint-stylish'));
 });
 
+
+gulp.task('inject', function () {
+    var target = gulp.src('./index.html');
+
+    var csssources = gulp.src(assets.core.css, {read: false});
+    var jssources = gulp.src(assets.core.js, {read: false});
+
+    return target
+        .pipe(inject(csssources))
+        .pipe(inject(jssources))
+        .pipe(gulp.dest('./'));
+});
+
 gulp.task('clean', function () {
     return gulp.src('./dist', { read: false })
         .pipe(clean());
 });
 
 
-
-
-gulp.task('default', ['lint'], function() {
+gulp.task('default', ['lint', 'inject'], function() {
     // place code for your default task here
 });
